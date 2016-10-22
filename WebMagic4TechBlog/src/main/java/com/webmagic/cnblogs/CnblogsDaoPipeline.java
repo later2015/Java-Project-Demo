3687029670f2acc045d052229f7c078c122a7e42
later2015 <448273866@qq.com>
2016-10-22 16:30:09 +0800

    提交cnblogs,oschina爬虫代码

--- WebMagic4TechBlog/src/main/java/com/webmagic/cnblogs/CnblogsDaoPipeline.java
+++ WebMagic4TechBlog/src/main/java/com/webmagic/cnblogs/CnblogsDaoPipeline.java
@@ -1,4 +1,4 @@
-package com.webmagic.csdn;
+package com.webmagic.cnblogs;
 
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.pipeline.PageModelPipeline;
@@ -6,18 +6,18 @@ import us.codecraft.webmagic.pipeline.PageModelPipeline;
 /**
  * Created by liqiliang on 2016/10/23 9:35.
  */
-public class CsdnDaoPipeline implements PageModelPipeline<CsdnUser> {
+public class CnblogsDaoPipeline implements PageModelPipeline<CnblogsUser> {
 
 
 
-    public CsdnDaoPipeline() {
+    public CnblogsDaoPipeline() {
     }
 
-    public void process(CsdnUser oschinaUser, Task task) {
+    public void process(CnblogsUser oschinaUser, Task task) {
         //page.getUrl()
         task.getSite();
         System.out.println("获取到的结果："+oschinaUser.getTitle()+"  "+oschinaUser.getCount()+" 评论数："+oschinaUser.getCommentCount()+" site:"+task.getSite());
-//        CsdnDao oschinaDao = new CsdnDaoImpl();
+//        CnblogsDao oschinaDao = new CnblogsDaoImpl();
 //        int row = oschinaDao.saveUser(oschinaUser);
         System.out.println("###保存成功###" + oschinaUser.toString());
     }
--- WebMagic4TechBlog/src/main/java/com/webmagic/oschina/OschinaDaoPipeline.java
+++ WebMagic4TechBlog/src/main/java/com/webmagic/oschina/OschinaDaoPipeline.java
@@ -1,4 +1,4 @@
-package com.webmagic.csdn;
+package com.webmagic.oschina;
 
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.pipeline.PageModelPipeline;
@@ -6,18 +6,18 @@ import us.codecraft.webmagic.pipeline.PageModelPipeline;
 /**
  * Created by liqiliang on 2016/10/23 9:35.
  */
-public class CsdnDaoPipeline implements PageModelPipeline<CsdnUser> {
+public class OschinaDaoPipeline implements PageModelPipeline<OschinaUser> {
 
 
 
-    public CsdnDaoPipeline() {
+    public OschinaDaoPipeline() {
     }
 
-    public void process(CsdnUser oschinaUser, Task task) {
+    public void process(OschinaUser oschinaUser, Task task) {
         //page.getUrl()
         task.getSite();
         System.out.println("获取到的结果："+oschinaUser.getTitle()+"  "+oschinaUser.getCount()+" 评论数："+oschinaUser.getCommentCount()+" site:"+task.getSite());
-//        CsdnDao oschinaDao = new CsdnDaoImpl();
+//        OschinaDao oschinaDao = new OschinaDaoImpl();
 //        int row = oschinaDao.saveUser(oschinaUser);
         System.out.println("###保存成功###" + oschinaUser.toString());
     }
