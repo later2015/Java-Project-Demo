06a5f854d5c493864f1735b117522e7511363a0d
later2015 <448273866@qq.com>
2016-10-24 17:19:47 +0800

    提交iteye爬虫代码

---
+++ WebMagic4TechBlog/src/main/java/com/webmagic/iteye/IteyeDaoPipeline.java
@@ -0,0 +1,25 @@
+package com.webmagic.iteye;
+
+import us.codecraft.webmagic.Task;
+import us.codecraft.webmagic.pipeline.PageModelPipeline;
+
+/**
+ * Created by liqiliang on 2016/10/23 9:35.
+ */
+public class IteyeDaoPipeline implements PageModelPipeline<IteyeUser> {
+
+
+
+    public IteyeDaoPipeline() {
+    }
+
+    public void process(IteyeUser oschinaUser, Task task) {
+        //page.getUrl()
+        System.out.println("获取到的结果："+oschinaUser.getTitle()+"  "+oschinaUser.getCount()+" 评论数："+oschinaUser.getCommentCount());
+//        OschinaDao oschinaDao = new OschinaDaoImpl();
+//        int row = oschinaDao.saveUser(oschinaUser);
+        System.out.println("###保存成功###" + oschinaUser.toString());
+    }
+
+
+}
---
+++ WebMagic4TechBlog/src/main/java/com/webmagic/iteye/IteyeUser.java
@@ -0,0 +1,153 @@
+package com.webmagic.iteye;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.model.AfterExtractor;
+import us.codecraft.webmagic.model.OOSpider;
+import us.codecraft.webmagic.model.annotation.ExtractBy;
+import us.codecraft.webmagic.model.annotation.HelpUrl;
+import us.codecraft.webmagic.model.annotation.TargetUrl;
+import us.codecraft.webmagic.monitor.SpiderMonitor;
+
+import javax.management.JMException;
+
+/**
+ * Created by liqiliang on 2016/10/22 10:54.
+ */
+
+//@TargetUrl({"http://[a-z0-9]+.iteye.com/blog/\\w+","http://[a-z0-9]+.iteye.com/blog/\\w+"})
+@TargetUrl("http://[a-z0-9]+.iteye.com/blog/\\w+")
+@HelpUrl("http://www.iteye.com/blogs")
+public class IteyeUser implements AfterExtractor {
+
+    //标题
+    @ExtractBy(value = "//div[@class='blog_title']/h3/a/text()",notNull = true)
+    private String title;
+
+    //阅读数
+    @ExtractBy(value = "//div[@class='blog_bottom']/ul/li[2]/text()",notNull = false)
+    private String count;
+
+    //文章内容
+    @ExtractBy(value = "//div[@id='blog_content']",notNull = true)//如果里面内容还有节点的，不能使用text(),会获取不到内容
+    private String content;
+
+    //原文URL TODO 没写好的xpath一定要把notNull set为false，设为true的话，他一直不match，一直没数据，就会一直不触发那个pipeline
+    private String fromUrl;
+
+    //评论数
+    @ExtractBy(value = "//div[@class='blog_bottom']/ul/li[3]/a/text()",notNull = false)
+    private String commentCount;
+
+    //标签
+    @ExtractBy(value = "//div[@class='news_tag']/a[1]/text()",notNull = false)
+    private String tags1;
+    @ExtractBy(value = "//div[@class='news_tag']/a[2]/text()",notNull = false)
+    private String tags2;
+    @ExtractBy(value = "//div[@class='news_tag']/a[3]/text()",notNull = false)
+    private String tags3;
+    @ExtractBy(value = "//div[@class='news_tag']/a[4]/text()",notNull = false)
+    private String tags4;
+    //分类
+    @ExtractBy(value = "//ul[@class='blog_categories']/li/a/text()",notNull = false)
+    private String category;
+
+
+    public void afterProcess(Page page) {
+        setFromUrl(String.valueOf(page.getUrl()));
+        System.out.println("URL::"+page.getUrl());
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getFromUrl() {
+        return fromUrl;
+    }
+
+    public void setFromUrl(String fromUrl) {
+        this.fromUrl = fromUrl;
+    }
+
+    public String getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(String commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public String getTags1() {
+        return tags1;
+    }
+
+    public void setTags1(String tags1) {
+        this.tags1 = tags1;
+    }
+
+    public String getTags2() {
+        return tags2;
+    }
+
+    public void setTags2(String tags2) {
+        this.tags2 = tags2;
+    }
+
+    public String getTags3() {
+        return tags3;
+    }
+
+    public void setTags3(String tags3) {
+        this.tags3 = tags3;
+    }
+
+    public String getTags4() {
+        return tags4;
+    }
+
+    public void setTags4(String tags4) {
+        this.tags4 = tags4;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+
+    public static void main(String[] args) throws JMException {
+
+        Spider spider = OOSpider.create(
+                Site.me().setRetryTimes(3).setSleepTime(1000),
+                new IteyeDaoPipeline(), IteyeUser.class)
+                .addUrl("http://cakin24.iteye.com/blog/2332401").thread(1);//https://my.oschina.net/u/2249566/blog/758673
+        SpiderMonitor.instance().register(spider);
+        spider.start();
+    }
+}
