5eb7d5abfa665a9a98243cacf5e0c480559fae41
later2015 <448273866@qq.com>
2016-10-25 15:30:51 +0800

    更新iteye爬虫代码

--- WebMagic4TechBlog/src/main/java/com/webmagic/iteye/IteyeDaoPipeline.java
+++ WebMagic4TechBlog/src/main/java/com/webmagic/iteye/IteyeDaoPipeline.java
@@ -15,7 +15,9 @@ public class IteyeDaoPipeline implements PageModelPipeline<IteyeUser> {
 
     public void process(IteyeUser oschinaUser, Task task) {
         //page.getUrl()
+        oschinaUser.setCount(oschinaUser.getCount().replace("浏览","").trim());
         System.out.println("获取到的结果："+oschinaUser.getTitle()+"  "+oschinaUser.getCount()+" 评论数："+oschinaUser.getCommentCount());
+
 //        OschinaDao oschinaDao = new OschinaDaoImpl();
 //        int row = oschinaDao.saveUser(oschinaUser);
         System.out.println("###保存成功###" + oschinaUser.toString());
