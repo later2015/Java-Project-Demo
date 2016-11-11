a9dbc1e2c7be6ed7af00dff95d9fb2d956a5a362
later2015 <448273866@qq.com>
2016-11-11 12:01:33 +0800

    提交Spring integration amqp的代码

--- Spring_Intergration_amqp/src/main/java/org/springframework/integration/samples/amqp/service/SubscriberServiceActivator.java
+++ Spring_Intergration_amqp/src/main/java/org/springframework/integration/samples/amqp/service/SubscriberServiceActivator.java
@@ -10,12 +10,25 @@ public class SubscriberServiceActivator {
 	private static Log logger = LogFactory
 			.getLog(SubscriberServiceActivator.class);
 //处理接收到的消息．ｘｍｌ里已经配置好了使用这个类去监听
-	public void logXml(Message<?> msg) throws Exception {
+	public void logXml_old(Message<?> msg) throws Exception {
 		logger.info("correlationId:::::"+msg.getHeaders().get("correlationId"));
 		if (msg.getPayload() instanceof byte[]){
 			logger.info(" PAYLOAD 11 ###" + new String((byte[])msg.getPayload()));
 		}else{
 			logger.info(" PAYLOAD 22 ###" + msg.getPayload());
 		}
+	}
+	//处理接收到的消息．ｘｍｌ里已经配置好了使用这个类去监听
+	public Message logXml(Message<?> msg) throws Exception {
+		System.out.println(msg.getHeaders().get("amqp_consumerQueue"));
+
+		logger.info("correlationId:::::"+msg.getHeaders().get("correlationId"));
+		if (msg.getPayload() instanceof byte[]){
+			logger.info(" PAYLOAD 11 ###" + new String((byte[])msg.getPayload()));
+		}else{
+			logger.info(" PAYLOAD 22 ###" + msg.getPayload());
+		}
+
+		return msg;
 	}
 }
