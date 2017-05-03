af28b3b47df3897fd89301bb0f5f39a1c7f66fb9
later2015 <448273866@qq.com>
2017-05-03 14:40:12 +0800

    添加二叉树遍历查找最大值的代码

--- lintcode/src/main/java/com/lintcode/easy/MaxNode.java
+++ lintcode/src/main/java/com/lintcode/easy/MaxNode.java
@@ -1,14 +1,17 @@
 package com.lintcode.easy;
+
+import java.util.ArrayList;
+
 /**
  * 在二叉树中寻找值最大的节点并返回。
  * 给出如下一棵二叉树：
-
-      1
-   /   \
- -5     2
- / \   /  \
- 0   3 -4  -5
- 返回值为 3 的节点。
+ * <p/>
+ * 1
+ * /   \
+ * -5     2
+ * / \   /  \
+ * 0   3 -4  -5
+ * 返回值为 3 的节点。
  * Created by liqiliang <liqiliang@baibu.la> on 2017/4/26.
  */
 public class MaxNode {
@@ -18,21 +21,29 @@ public class MaxNode {
      */
     public TreeNode maxNode(TreeNode root) {
         // Write your code here
-        //有bug，没能有效遍历
+        //先把二叉树遍历，放在一个list里
+        //然后for循环一次把二叉树取出来找最大值即可。
         TreeNode maxNode;
-        if (root==null)
+        if (root == null)
             return null;
-        if (maxNode(root.left)!=null && maxNode(root.right)!=null) {
-             maxNode = maxNode(root.left).val > maxNode(root.right).val ? maxNode(root.left) : maxNode(root.right);
-        }else if (maxNode(root.left)==null && maxNode(root.right)!=null){
-            maxNode=maxNode(root.right);
-        }else if (maxNode(root.right)==null && maxNode(root.left)!=null){
-            maxNode=maxNode(root.left);
-        }else {
-            maxNode=root;
+        ArrayList<TreeNode> result = new ArrayList<TreeNode>();
+        helper(root, result);
+        maxNode = result.get(0);
+        for (TreeNode item : result) {
+            if (maxNode.val < item.val)
+                maxNode = item;
         }
         return maxNode;
     }
 
-
+    //递归法前序遍历二叉树，遍历结果放到ArrayList里
+    public ArrayList<TreeNode> helper(TreeNode root, ArrayList<TreeNode> result) {
+        if (root == null) {
+            return result;
+        }
+        helper(root.left, result);
+        helper(root.right, result);
+        result.add(root);
+        return result;
+    }
 }
